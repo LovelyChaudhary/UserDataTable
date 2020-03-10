898d11422a81436dd4e87feb22d7b474ed1c301e
LovelyChaudhary <LovelyChaudhary@users.noreply.github.com>
2020-03-11 04:15:15 +0530

    commit 1-base project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { HomeResolver } from './home/home.resolver';
+import { AuthGuardService } from './auth-guard.service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'login',component: LoginComponent},
+  {path :'register',component: RegisterComponent},
+  {path :'home',component: HomeComponent,  canActivate: [AuthGuardService] ,
+  resolve:{resolveData :HomeResolver}},
+  {path :'**',redirectTo:'register',pathMatch:'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[HomeResolver]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { HomeService } from './home.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [HomeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
